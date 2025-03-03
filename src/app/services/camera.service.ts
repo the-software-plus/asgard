7616abf8ad2da2308a8682ba31f2679052dd32c8
jlmcavalcante <joseluismoreiracavalcante@gmail.com>
2025-03-03 20:33:05 -0300

    camera service

---
+++ src/app/services/camera.service.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@angular/core';
+import { Camera, CameraResultType, CameraSource, Photo } from '@capacitor/camera';
+import { Filesystem, Directory } from '@capacitor/filesystem';
+import { Preferences } from '@capacitor/preferences';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CameraService {
+  public photos: PlantPhoto[] = [];
+
+  constructor() { }
+
+  public async addNewToGallery() {
+    // Take a photo
+    const capturedPhoto = await Camera.getPhoto({
+      resultType: CameraResultType.Uri,
+      source: CameraSource.Camera,
+      allowEditing: true,
+      quality: 100
+    });
+
+    this.photos.unshift({
+      filepath: "soon...",
+      webviewPath: capturedPhoto.webPath!
+    });
+  }
+}
+
+export interface PlantPhoto {
+  filepath: string;
+  webviewPath?: string;
+}
