b246442fd06825cbec087f3f49f000b44016dd1a
jlmcavalcante <joseluismoreiracavalcante@gmail.com>
2025-03-30 00:53:40 -0300

    Create login page and fix routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,15 @@ import { Routes } from '@angular/router';
 export const routes: Routes = [
   {
     path: '',
-    loadChildren: () => import('./tabs/tabs.routes').then((m) => m.routes),
+    redirectTo: 'login',
+    pathMatch: 'full',
+  },
+  {
+    path: 'login',
+    loadComponent: () => import('./pages/login/login.page').then( m => m.LoginPage)
+  },
+  {
+    path: 'tabs',
+    loadChildren: () => import('./pages/tabs/tabs.routes').then((m) => m.routes),
   },
 ];
