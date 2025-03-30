1b6a65040bed204a707dc68062d41c8bdeb402b2
jlmcavalcante <joseluismoreiracavalcante@gmail.com>
2025-03-30 01:17:13 -0300

    implements authGuard and adds it to the tabs route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,4 +1,5 @@
 import { Routes } from '@angular/router';
+import { authGuard } from './guards/auth.guard';
 
 export const routes: Routes = [
   {
@@ -12,6 +13,7 @@ export const routes: Routes = [
   },
   {
     path: 'tabs',
+    canActivate: [authGuard],
     loadChildren: () => import('./pages/tabs/tabs.routes').then((m) => m.routes),
   },
 ];
