86b6c16f508d44f8ba397b6ac041aa65db02df4a
jlmcavalcante <joseluismoreiracavalcante@gmail.com>
2025-03-30 01:12:29 -0300

    Implements the login logic on page

--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -1,20 +1,51 @@
-import { Component, OnInit } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { IonicModule, AlertController, LoadingController } from '@ionic/angular';
+import { CommonModule } from '@angular/common';
+import { AuthService } from 'src/app/services/auth.service';
+import { catchError } from 'rxjs/operators';
+import { of } from 'rxjs';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.page.html',
   styleUrls: ['./login.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [IonicModule, CommonModule, FormsModule],
+
 })
-export class LoginPage implements OnInit {
+export class LoginPage {
+  email = '';
+  password = '';
 
-  constructor() { }
+  constructor(
+    private authService: AuthService,
+    private router: Router,
+    private alertController: AlertController,
+    private loadingController: LoadingController
+  ) {}
 
-  ngOnInit() {
+  async login() {
+    const loading = await this.loadingController.create({
+      message: 'Autenticando...',
+    });
+    await loading.present();
+
+    this.authService.login(this.email, this.password).subscribe({
+      next: () => {
+        loading.dismiss();
+        this.router.navigateByUrl('/tabs', { replaceUrl: true });
+      },
+      error: async () => {
+        loading.dismiss();
+        const alert = await this.alertController.create({
+          header: 'Erro',
+          message: 'E-mail ou senha inválidos.',
+          buttons: ['OK'],
+        });
+        await alert.present();
+      },
+    });
   }
-
 }
